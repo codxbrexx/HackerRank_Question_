a96f0566ea333e72a0ab789b1a648847615778c0
mozammilali01 <mozammilali2018@gmail.com>
2024-08-03 23:32:00 +0530

    Add files via upload

---
+++ Day_3.py
@@ -0,0 +1,24 @@
+# Question == 3
+
+# if __name__ == '__main__':
+#     a = int(input("Enter 1st numer :"))
+#     b = int(input("Enter 2nd number :"))
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+    
+    
+# Questoin == 4
+
+if __name__ == '__main__':
+    a = int(input("Enter 1st Number :"))
+    b = int(input("Enter 2nd Number :"))
+    print(a//b)   #remmender
+    print(a/b)    #divide
+    
+    
+    
+    
+    
+    
+    
