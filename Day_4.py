1b2eeb458f57217934bde2cdaa9254423762950e
mozammilali01 <mozammilali2018@gmail.com>
2024-08-03 23:32:15 +0530

    Add files via upload

---
+++ Day_4.py
@@ -0,0 +1,16 @@
+#question == 5
+
+# if __name__ == '__main__':
+#     n = int(input())
+    
+# for i in range(n):
+#     print(i**2)
+    
+    
+#question == 6
+
+if __name__ == '__main__':
+    n = int(input("Enter a Number :"))
+    
+for i in range(n):
+    print(i+1, end = "")
